0dc3311c6082bd58429b7c9b1df79bdd442a7bb0
Miles Wang <wangxunn@gmail.com>
2013-09-12 14:26:35 -0700

    change for multiple level, and add comment selection

--- js/controllers.js
+++ js/controllers.js
@@ -35,17 +35,16 @@ function ParagraphController($scope, $http){
     $scope.active = function(p){
         if ($scope.activePid === p.id){
             $scope.activePid = null;
-            p.cls="";
         }else{
             $scope.activePid = p.id;
-            p.cls="active";
         }
     };
 }
 
 function CommentsController($scope, $http){
     $scope.init = function(){
-        $scope.comments = [];
+        $scope.activeComment = null;
+        $scope.comment_list = [];
         $http({
             method: 'GET',
             url: requestURL2,
@@ -58,32 +57,43 @@ function CommentsController($scope, $http){
         })
             .success(function(data, status, headers, config){
                 var temp = {};
+                console.log(data);
                 for (var i = 0; i < data["record"].length; i ++){
-                    var comment = data["record"][i]["SRPComments_by_comment_id"];
-                    comment.comments=[];
-                    temp[comment.id] = comment;
-                    if (comment["parent_id"] === null){
-                        $scope.comments.push(comment);
+                    var c = data["record"][i]["SRPComments_by_comment_id"];
+                    console.log(c.id, c);
+                    temp[c.id] = c;
+                    if (c["parent_id"] === null){
+                        c.level = 0;
+                        $scope.comment_list.push(c);
                     }else{
-                        temp[comment["parent_id"]].comments.push(comment);
+                        c.level = temp[c["parent_id"]].level + 1;
+                        var parent_index = $scope.comment_list.indexOf(temp[c["parent_id"]]);
+                        $scope.comment_list.splice(parent_index + 1, 0, c);
                     }
+
                 }
+                console.log($scope.comment_list);
             })
             .error(function(){
                 console.log(arguments);
             });
     };
     $scope.addComment = function (newCommentText) {
-        if (!newCommentText) return;
-        $scope.comments.push({text: newCommentText, isRemoved: false, active: false});
-        $scope.newComment = "";
+        //if (!newCommentText) return;
+        //$scope.comments.push({text: newCommentText, isRemoved: false, active: false});
+        //$scope.newComment = "";
     };
     $scope.removeComment = function(comment){
         var index = $scope.comments.indexOf(comment);
         $scope.comments.splice(index, 1)
     };
-    $scope.activeComment = function(comment){
-        comment.active = !comment.active;
+    $scope.active = function(comment){
+        if ($scope.activeComment === comment.id){
+            $scope.activeComment = null;
+        }else{
+            $scope.activeComment = comment.id;
+        }
+        console.log($scope.activeComment,comment);
     };
 
 
