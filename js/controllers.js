d5726ebb8e2f186be117afbe48774f5563d0bfe4
Miles Wang <wangxunn@gmail.com>
2013-09-11 18:40:06 -0700

    change layout

--- js/controllers.js
+++ js/controllers.js
@@ -5,23 +5,41 @@
  * Time: 12:20 PM
  */
 
-function CommentsController($scope){
-    $scope.comments = [];
-    $scope.numberOfComments = 0;
-    $scope.addComment = function (newCommentText) {
-        $scope.comments.push({text: newCommentText, isRemoved: false, active: false});
-        $scope.newComment = "";
-        $scope.numberOfComments += 1;
-        console.log(!$scope.numberOfComments);
-    };
-    $scope.removeComment = function(comment){
-        comment.isRemoved = true;
-        $scope.numberOfComments -= 1;
-    };
-    $scope.activeComment = function(comment){
-        comment.active = !comment.active;
-    };
+function ParagraphController($scope, $http){
+    $http.get('paragraphs.json').success(function(data, status, headers, config){
+        $scope.activePid = null;
+        $scope.paragraphs = data.paragraphs;
+        $scope.toggleComments = function(p){
+            if ($scope.activePid === p.pid){
+                $scope.activePid = null;
+                p.cls="";
+            }else{
+                $scope.activePid = p.pid;
+                p.cls="active";
+            }
+        };
 
+    });
 
-    $scope.addComment("this is a test comment");
 }
+//
+//function CommentsController($scope){
+//    $scope.comments = [];
+//    $scope.numberOfComments = 0;
+//    $scope.addComment = function (newCommentText) {
+//        $scope.comments.push({text: newCommentText, isRemoved: false, active: false});
+//        $scope.newComment = "";
+//        $scope.numberOfComments += 1;
+//        console.log(!$scope.numberOfComments);
+//    };
+//    $scope.removeComment = function(comment){
+//        comment.isRemoved = true;
+//        $scope.numberOfComments -= 1;
+//    };
+//    $scope.activeComment = function(comment){
+//        comment.active = !comment.active;
+//    };
+//
+//
+//    $scope.addComment("this is a test comment");
+//}
